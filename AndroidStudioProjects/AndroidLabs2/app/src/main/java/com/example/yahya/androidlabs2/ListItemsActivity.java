9ba12937d6ed6338ba64cd6ce22148d3bd9ff079
yahya <awad0083@algonquinlive.com>
2017-10-03 22:48:48 -0400

    Finished lab 3

--- AndroidStudioProjects/AndroidLabs2/app/src/main/java/com/example/yahya/androidlabs2/ListItemsActivity.java
+++ AndroidStudioProjects/AndroidLabs2/app/src/main/java/com/example/yahya/androidlabs2/ListItemsActivity.java
@@ -1,13 +1,138 @@
 package com.example.yahya.androidlabs2;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class ListItemsActivity extends Activity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_items);
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+
+        ImageButton imageButton = (ImageButton)findViewById(R.id.imageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+        //****************************************************************************************************//
+        /**Switch obejct functions */
+        Switch toggle = (Switch) findViewById(R.id.switch1);
+        toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    Toast.makeText(ListItemsActivity.this,"Switch is On",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(ListItemsActivity.this,"Switch is Off",Toast.LENGTH_LONG).show();                }
+            }
+        });
+
+        //****************************************************************************************************//
+
+        /**Checkbox obejct functions */
+        CheckBox check = (CheckBox)findViewById(R.id.checkBox);
+        check.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+                // 1. Instantiate an AlertDialog.Builder with its constructor
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListItemsActivity.this);
+                // 2. Chain together various setter methods to set the dialog characteristics
+                builder.setMessage("Finish the activity?") //Add a dialog message to strings.xml
+                        .setTitle("Inform")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent resultIntent = new Intent(  );
+                                resultIntent.putExtra("Response", "Here is my response");
+                                setResult(Activity.RESULT_OK, resultIntent);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        })
+                        .show();
+
+            }
+        });
+    }
+
+    //****************************************************************************************************//
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            ImageButton cameraImage = (ImageButton)findViewById(R.id.imageButton);
+
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            cameraImage.setImageBitmap(imageBitmap);
+        }
+    }
+    //****************************************************************************************************//
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    //****************************************************************************************************//
+    protected void onStart(){
+        super.onStart();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onRestart()");
+    }
+
+    protected void onResume(){
+        super.onResume();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    protected void onPause(){
+        super.onPause();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    protected void onStop(){
+        super.onStop();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+        final String ACTIVITY_NAME = "ListItemsActivity";
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
